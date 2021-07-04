ba2da25005bf31271f854fe84b41844d75622a30
moonhunter <vdcloayza@gmail.com>
2021-07-03 23:50:52 -0400

    cambios

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>MIREN PUTOS
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,3 +20,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
